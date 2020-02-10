a6d951731ebe06de88b16421a747f9778e7323ab
Teerawat Vanasapdamrong <teerawat.vanasapdamrong@scb.co.th>
2020-02-10 16:28:47 +0700

    initial commit

---
+++ GithubUsers/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  GithubUsers
+//
+//  Created by Teerawat Vanasapdamrong on 9/2/20.
+//  Copyright © 2020 mrhangz. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
